5de87eaf5c83bd91b630aebe6387b24ca13dd33f
Felipe Volcov Nambara <fpfernandesnambara@gmail.com>
2012-11-27 21:16:06 -0200

    first-commit

---
+++ src/projetomatematicadiscreta/ProjetoMatematicaDiscreta.java
@@ -0,0 +1,38 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package projetomatematicadiscreta;
+
+/**
+ *
+ * @author Felipe V Nambara
+ */
+public class ProjetoMatematicaDiscreta {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+    public int grauGrafoNaoDirigido(int[][] grafo){                        
+        int grau = 0;
+        
+        for (int i = 0; i < grafo.length; i++) {
+            
+            for (int j = 0; j < grafo[i].length; j++) {
+                
+                if(){
+                    
+                }
+                
+                
+            }
+            
+        }
+        
+        return grau;
+    }
+}
